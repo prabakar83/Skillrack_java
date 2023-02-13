bfe3d0541141f49a9fe537d44d8c1e7ef14cb0da
prabakar83 <prabaprabhu883@gmail.com>
2023-02-13 20:45:25 +0530

    First commit

---
+++ input 100/Sr.java
@@ -0,0 +1,20 @@
+
+
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a=100 || b=100){
+		    System.out.println("yes");
+		}
+		else{
+		    System.out.println("no");
+		}
+
+
+	}
+}
