830af8eaba18ff51376d438fa03978e343771cc5
prabakar83 <prabaprabhu883@gmail.com>
2023-02-16 22:20:17 +0530

    skillrack

---
+++ canteen expenses/ans.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class Hello {
+     public static void main (String] args) {
+        Scanner sc = new Scanner( (System.in);
+         int a = sc.nextInt();
+        Systen.out.print(100 - a);
---
+++ multiply two number/ans.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Hello {
+     public static void main(String[] args) {
+         Scanner SC = new Scanner (System.in);
+         int num1 = sc.nextInt();
+         int num2 = sc.nextInt();
+        System.out.print( (num1 * num2);
