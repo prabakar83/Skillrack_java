af010bb5303f53a0d54e7cc679cf7e2944b41e9e
prabakar83 <prabaprabhu883@gmail.com>
2023-02-16 22:39:09 +0530

    feb16

---
+++ middle three characters/ans.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.println(a.substring(2,5));
+
+
+	}
+}
